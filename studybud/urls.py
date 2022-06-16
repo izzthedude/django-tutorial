090fc8a4ba8ffa8c2615b011ce0241acbe4acd2b
Izzat Z <izzat.zainir11@gmail.com>
2022-06-16 17:46:31 +0800

    Start creating basic API

--- studybud/urls.py
+++ studybud/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from base import api
+from base.api import urls
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('base.urls'))
+    path('', include('base.urls')),
+    path("api/", include('base.api.urls'), name="api"),
 ]
